d2bd856210edf8da8dbfcdb7f8b901e4f92c5169
Daniel Clarke <u8sand@gmail.com>
2018-01-16 17:50:28 -0500

    Put common ENV variables in config.sh

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export BACKUP_ROOT=/
+export BACKUP=backup.tar.gz
+export BACKUP_DIR=backup
+export WHITELIST_DIR=whitelist.d
+export BLACKLIST_DIR=blacklist.d
+export HOOKS_DIR=hooks.d
+
+handle_hook_dir() {
+  DIR=$1
+  if [ -d "${DIR}" ]; then
+    find "${DIR}" -type f -regex ".*\.sh" -exec bash {} \;
+  fi
+}
+
+# Custom exports will be available in hooked scripts
+# export MY_CUSTOM_VARIABLE="my_value"
--- restore.sh
+++ restore.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-export BACKUP_ROOT=/
-export BACKUP=backup.tar.gz
-export HOOKS_DIR=hooks.d
+if [ "$UID" != "0" ]; then
+  echo "You must run this script as root." 1>&2
+  exit 1
+fi
 
-handle_hook_dir() {
-  DIR=$1
-  if [ -d "${DIR}" ]; then
-    find "${DIR}" -type f -regex ".*\.sh" -exec {} \;
-  fi
-}
+SRCDIR=$(dirname "$(realpath $0)")
+cd ${SRCDIR}
+source config.sh
 
 handle_hook_dir "${HOOKS_DIR}/before"
 handle_hook_dir "${HOOKS_DIR}/before-restore"
+
 tar -xf ${BACKUP} -C ${BACKUP_ROOT}
+
 handle_hook_dir "${HOOKS_DIR}/after-restore"
 handle_hook_dir "${HOOKS_DIR}/after"
