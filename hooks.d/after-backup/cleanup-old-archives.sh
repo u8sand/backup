17d749a79ce7299be9e011e7bd5ea5e7a594afe0
Daniel Clarke <u8sand@gmail.com>
2018-01-30 17:10:32 -0500

    Escaped environment variables in archive script

--- hooks.d/after-backup/cleanup-old-archives.sh
+++ hooks.d/after-backup/cleanup-old-archives.sh
@@ -6,10 +6,10 @@ ls ${BACKUP_DIR}/archives/ \
   | awk 'BEGIN {
   split(ENVIRON["TODAY"], "-", TODAY);
   CUTOFF = (TODAY[0]-1)"-"TODAY[1]"-"TODAY[2];
-} $0 <= CUTOFF {
-  print $0
+} \$0 <= CUTOFF {
+  print \$0
 }' | while read DIR; do
-  (>&2 echo "removing old archive ${DIR}...")
-  rm -r ${BACKUP_DIR}/archives/${DIR}
+  (>&2 echo "removing old archive \${DIR}...")
+  rm -r ${BACKUP_DIR}/archives/\${DIR}
 done
 EOF
